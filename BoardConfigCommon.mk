f6dda6d5763a35c88a745a7dcd26549f7dafa419
underscoremone <underscoremone@gmail.com>
2017-01-03 06:48:02 +0000

    trlte: add kernel toolchain
    and change PRODUCT_AAPT_PREF_CONFIG
    
    Change-Id: Ifadb898b17d2ed6742d44f0c98c78424e1b582dd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,6 +79,8 @@ TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := apq8084_sec_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/trlte
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
--- trlte.mk
+++ trlte.mk
@@ -27,7 +27,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := 560dpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # Boot animation
