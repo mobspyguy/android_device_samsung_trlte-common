4b30debc817e6a57eea73731b9e88ea52669278e
elektroschmock <elektroschmock78@googlemail.com>
2017-02-10 20:28:57 +0000

    trlte: Use phone-xxhdpi-3072-hwui-memory config * Fixes out of memory crashes of G+ and other apps
    Change-Id: Id634b0f82cb259d6adf93cfb5373b153fb16fa27

--- trlte.mk
+++ trlte.mk
@@ -39,7 +39,7 @@ PRODUCT_PROPERTY_OVERRIDES += 	dalvik.vm.heapgrowthlimit=256m \
 				persist.radio.apm_sim_not_pwdn=1
 
 $(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
