0174dc576e430bbb65ab9be67c228cf27820d4d8
Luca Stefani <luca020400@lineageos.org>
2017-06-25 12:56:07 +0200

    trlte-common: set device dependent BT name
    Change-Id: Ic01e2aa8ee69e41e8a3c0fecd15e122b44a2fed1

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,24 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Note 4"
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char* BtmGetDefaultName()
+{
+    char product_device[PROPERTY_VALUE_MAX];
+    property_get("ro.product.device", product_device, "");
+
+    if (strstr(product_device, "trlte"))
+        return "Samsung Galaxy Note 4";
+    if (strstr(product_device, "tblte"))
+        return "Samsung Galaxy Note Edge";
+
+    // Fallback to ro.product.model
+    return "";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 
 #define BTE_BLE_STACK_CONF_FILE "/etc/bluetooth/bt_stack.conf"
 
