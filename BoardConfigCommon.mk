8d483b72a3cb1b1259592e9f89d3b104aee17ed7
Michael Bestas <mkbestas@lineageos.org>
2017-12-21 01:35:00 +0200

    trlte-common: Include legacy qcom sepolicy rules
    Change-Id: Ic499de98c34a63de18e935d2cc6f23f0e69b1a43

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,6 +59,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy
