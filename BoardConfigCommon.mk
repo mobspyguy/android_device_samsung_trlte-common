90d0dc6e00a30c58df1b80bec85d2c943b384b7a
Lukas Berger <mail@lukasberger.at>
2018-01-19 23:08:09 +0100

    trlte-common: applied update for WiFi
      * Update for wpa_supplicant
    
      * Set correct permissions for WiFi's firmware_path and nvram_path
    
      * Updated packages for WiFi (still wifiloader in /system before hardware/samsung/wifiloader was change for vendor)
    
    Change-Id: If20f56bf1a88cda426d86ebe8551cd035efe6723

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,6 +74,7 @@ BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WPA_SUPPLICANT_USE_HIDL     := true
 WIFI_BAND := 802_11_ABG
 WIFI_DRIVER_MODULE_ARG      := "firmware_path=/vendor/etc/wifi/bcmdhd_sta.bin nvram_path=/vendor/etc/wifi/nvram_net.txt"
 WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/vendor/etc/wifi/bcmdhd_apsta.bin nvram_path=/vendor/etc/wifi/nvram_net.txt"
