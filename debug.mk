e890bf69b6e4cbaa2aee2a312718051a47bb9ccb
mobspyguy <mobspyguy@gmail.com>
2019-01-14 18:36:02 +0100

    trlte-common: debug: add addional adb log properties
    Change-Id: I66750e88a78160259c407fc2cd5aeb5da51b6eae

--- debug.mk
+++ debug.mk
@@ -1,6 +1,7 @@
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.service.adb.enable=1 \
-    persist.sys.usb.config=adb \
+    persist.service.debuggable=1 \
+    persist.sys.usb.config=mtp,adb \
     ro.adb.secure=0 \
     ro.debuggable=1 \
     ro.secure=0
