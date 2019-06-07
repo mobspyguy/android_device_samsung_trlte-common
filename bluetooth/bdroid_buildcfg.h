0fb81748e4243fe2d18ab18d25357826bb2be642
ripee <ripee.dev@gmail.com>
2019-06-07 21:25:25 +0200

    trlte-common: Revise BTM_DEF_LOCAL_NAME more precisely

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Note"
+#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Note"
 
 #define MAX_ACL_CONNECTIONS    16
 #define MAX_L2CAP_CHANNELS    16
